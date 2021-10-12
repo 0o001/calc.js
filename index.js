65c1569c78d27ac75b1d8dad2b1d32f21cc409c0
Mustafa Uzun <mustafauzun0@gmail.com>
2021-10-12 11:39:58 +0300

    Update index.js
    + add more regex for operation string

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ function calc(operation) {
     //if parenthesis on the side of the digit then replace
     //etc. 1(2+2) to 1*(2+2)
     operation = operation.replace(/(-?\d)(\()/g, '$1*$2')
+
+    //etc. (2+2)1 to (2+2)*1
+    operation = operation.replace(/(-?\))(\d)/g, '$1*$2')
+
+    //etc. --+ to +
+    operation = operation.replace(/\-{2}/g, '+')
+
+    //etc. ++ more operation letter to one +
+    operation = operation.replace(/\+{2,}/, '+')
     
     return Function(`return +${operation}`)()
 
